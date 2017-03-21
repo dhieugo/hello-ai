17c480cf5854aad581e0a4c24af417c841b3a217
Hieu Doan <hieu.doan@edge-works.net>
2017-03-21 15:26:42 +0700

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+python 2.7
+
+Download training data from link below
+
+http://download.tensorflow.org/example_images/flower_photos.tgz
+
+Run retrain
+
+python retrain --bottleneck_dir=tmp/bottlenecks --how_many_training_steps 500 --model_dir=tmp/inception --output_graph=output/retrained_graph.pb --output_labels=output/retrained_labels.txt --image_dir=flower_photos/
---
+++ flowerdetection.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import sys
+import os
+
+
+# change this as you see fit
+# Loads label file, strips off carriage return
+
+class FlowerDetection():
+    def __init__(self):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.label_lines = [line.rstrip() for line
+                            in tf.gfile.GFile(os.path.join(self.dir_path, "output/retrained_labels.txt"))]
+        self.create_graph()
+        self.sess = tf.Session()
+
+    def create_graph(self):
+        with tf.gfile.FastGFile(os.path.join(self.dir_path, "output/retrained_graph.pb"), 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
+
+    def run(self, image_path):
+        image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = self.sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = self.sess.run(softmax_tensor, \
+                                    {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        print('Result for %s' % (image_path))
+        ret = ''
+        for node_id in top_k:
+            human_string = self.label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+            if (score > 0.5):
+                ret += human_string
+
+        return ret
+
+
